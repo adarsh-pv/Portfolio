8343fac5380f5259a4d5aed169d92c839b1103cd
adarsh-pv <adarshharindran112@gmail.com>
2024-10-16 13:13:43 +0530

    build-bug-resolved

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      unoptimized: true, // Only if using static hosting
+    },
+  };
+  
+  export default nextConfig;
 // /** @type {import('next').NextConfig} */
 // const nextConfig = {
 //     output: 'export',  // Enables static export
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,16 +7,16 @@ const HomePage = () => {
         {" "}
         {/* Ensures the content is centered and responsive */}
         <h1 className="text-2xl sm:text-4x2 md:text-clip font-bold mb-4">
-          Hi, I’m Adarsh👋🏼!
+          Hi, I&#39;m Adarsh👋🏼!
         </h1>
         <p className="text-lg mb-4 text-start">
-          Self taught web developer love to learn new things and I'm always
-          looking for new challenges to solve 😊<br/>
+          Self-taught web developer, love to learn new things and I&#39;m always
+          looking for new challenges to solve 😊<br />
           <a href="/contact" className="underline">connect now!</a>
         </p>
         {/* <p className="text-lg mb-4 text-start">
-          I’m always open to collaboration and love tackling challenging
-          problems. If you’re interested in working together or just want to
+          I&#39;m always open to collaboration and love tackling challenging
+          problems. If you&#39;re interested in working together or just want to
           chat about tech, feel free to reach out!
         </p> */}
       </div>
